f4ba35a7e237ea4ad3ab587b7f703b82dc694653
gatesma <gatesma@tencent.com>
2025-03-02 16:04:37 +0800

    测试调整

--- android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
+++ android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
@@ -1,6 +1,7 @@
 package com.gdelataillade.alarm.services
 
 import android.content.Context
+import android.media.AudioAttributes
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
@@ -9,14 +10,27 @@ class VibrationService(context: Context) {
     private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
     fun startVibrating(pattern: LongArray, repeat: Int) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            // Use VibrationEffect for API 26 and above
+//            val vibrationEffect = VibrationEffect.createWaveform(pattern, repeat)
+//            vibrator.vibrate(vibrationEffect)
+//        } else {
+//            // Use the older vibrate method for below API 26
+//            vibrator.vibrate(pattern, repeat)
+//        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Use VibrationEffect for API 26 and above
-            val vibrationEffect = VibrationEffect.createWaveform(pattern, repeat)
-            vibrator.vibrate(vibrationEffect)
+            vibrator.vibrate(VibrationEffect.createWaveform(pattern, repeat),
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+            )
         } else {
             // Use the older vibrate method for below API 26
             vibrator.vibrate(pattern, repeat)
         }
+
     }
 
     fun stopVibrating() {
